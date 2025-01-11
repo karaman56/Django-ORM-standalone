d8471f4500e949ea1328f37606233ea1aeabc9e9
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-01-12 00:33:44 +0500

    настроил

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 
 import django
-
+  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard
+
+passcards = Passcard.objects.all()
+active_passcards = Passcard.objects.filter(is_active=True)
+active_passcards_count = active_passcards.count()
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    print('Количество пропусков:', Passcard.objects.count())
+    print('Количество активных пропусков:',active_passcards_count)
